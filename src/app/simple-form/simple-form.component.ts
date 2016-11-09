e83e5e3f853ae8142aa94c9eb4257b452e0bf278
phaltoe <reverse9@live.com>
2016-11-09 08:06:44 -0200

    playing with inputs on Ang2

---
+++ src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  template: `<div>
+    <input #myInput type="text">
+    <button (click)="onClick(myInput.value)">Click me!</button>
+
+  </div>`,
+  styles: []
+})
+export class SimpleFormComponent implements OnInit {
+
+  onClick(input) {
+    console.log(input);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
